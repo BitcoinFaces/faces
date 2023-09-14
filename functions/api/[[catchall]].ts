e9142af36438e704b649887c813299bbc0e63966
Jason Schrader <whoabuddy@users.noreply.github.com>
2023-09-14 14:30:51 -0700

    docs: update catchall text

--- functions/api/[[catchall]].ts
+++ functions/api/[[catchall]].ts
@@ -1,9 +1,10 @@
 export async function onRequest(): Promise<Response> {
   return new Response(
     `Welcome to the Bitcoin Faces API! Supported endpoints below:
- - /api/get-svg-code?name=yourname.sats
- - /api/get-image?name=yourname.sats
- - /api/get-hash-array?name=yourname.sats
- - /api/get-layers?name=yourname.sats`
+ - /api/get-hash-array: returns the hash array for a given name, used to select the layers
+ - /api/get-image: returns the svg image for a given name
+ - /api/get-layers: returns the selected layers for a given name
+ - /api/get-layers-code: returns the svg code for each layer of a given name
+ - /api/get-svg-code: returns the full svg code for a given name`
   );
 }
