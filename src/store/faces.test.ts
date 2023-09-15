67fb48423c3575af6f2acd2e151688146a460e84
Jason Schrader <whoabuddy@users.noreply.github.com>
2023-09-15 10:05:42 -0700

    fix: update test naming and structure
    Catches an edge case where head-8 is intentionally skipped

--- src/store/faces.test.ts
+++ src/store/faces.test.ts
@@ -1,10 +1,17 @@
 import { INSCRIBED_ATTRIBUTES, LOCAL_ATTRIBUTES, Layer } from "./faces";
 
-describe("Attributes Match Test", () => {
+describe("Local attributes match on-chain attributes", () => {
   for (const [key, inscribedArray] of Object.entries(INSCRIBED_ATTRIBUTES)) {
     const layer = key as Layer;
     for (let i = 0; i < inscribedArray.length; i++) {
-      it(`should match ${layer}-${i + 1}`, async () => {
+      let layerId = "";
+      if (key === "head" && i > 6) {
+        // head-8 intentionally missing, too many embedded images
+        layerId = `${layer}-${i + 2}`;
+      } else {
+        layerId = `${layer}-${i + 1}`;
+      }
+      it(layerId, async () => {
         const hash = inscribedArray[i];
         if (hash !== undefined) {
           const response = await fetch(
@@ -18,9 +25,6 @@ describe("Attributes Match Test", () => {
             .replace(/"\s+\n/g, '"') // spaces and newlines following '"'
             .replace(/\n/g, " ") // all newlines to space
             .trim(); // leading and trailing whitespace
-          //console.log("returned SVG", data);
-          //console.log("cleaned SVG", cleanedSVG);
-          //console.log("local SVG", LOCAL_ATTRIBUTES[layer][i]);
           expect(cleanedSVG).toEqual(LOCAL_ATTRIBUTES[layer][i]);
         }
       });
