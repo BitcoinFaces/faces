147b80a6286cc673a2a4ba6719b2783aab14009e
Jason Schrader <whoabuddy@users.noreply.github.com>
2023-09-15 14:55:15 -0700

    chore: code cleanup

--- src/components/layout/bitcoin-faces-text-logo.tsx
+++ src/components/layout/bitcoin-faces-text-logo.tsx
@@ -6,7 +6,7 @@ const BitcoinFacesTextLogo = () => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g clip-path="url(#clip0_85_3835)">
+      <g clipPath="url(#clip0_85_3835)">
         <mask
           id="mask0_85_3835"
           style={{ maskType: "luminance" }}
