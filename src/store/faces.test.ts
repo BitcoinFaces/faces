ed03169d05051b321c372c42ae4847654a822058
Jason Schrader <whoabuddy@users.noreply.github.com>
2023-09-18 00:33:46 -0700

    fix: add test for keeping exports in sync

---
+++ src/store/faces.test.ts
@@ -0,0 +1,31 @@
+import fs from "fs";
+import path from "path";
+import * as Faces from "../components/faces";
+import { FACES_COMPONENTS } from "../store/faces";
+
+describe("Faces components", () => {
+  it("should keep FACES_COMPONENTS, exports and folder contents in sync", () => {
+    // Read filenames from 'faces' directory and filter .tsx files
+    const filePath = path.resolve(__dirname, "../components/faces");
+    const fileNames = fs
+      .readdirSync(filePath)
+      .filter((f) => f.endsWith(".tsx") && f !== "index.tsx");
+
+    // Convert filenames to export names
+    const expectedExports = fileNames.map(
+      (f) => "BitcoinFace" + f.replace("bitcoin-face-", "").replace(".tsx", "")
+    );
+
+    // Extract actual exports from index.ts
+    const actualExports = Object.keys(Faces).filter(
+      (f) => f !== "FACES_COMPONENTS"
+    );
+
+    // Validate exports against filenames
+    expect(actualExports.sort()).toEqual(expectedExports.sort());
+
+    // Validate FACES_COMPONENTS against exports
+    const actualComponents = FACES_COMPONENTS.map((c) => c.name);
+    expect(actualComponents.sort()).toEqual(actualExports.sort());
+  });
+});
